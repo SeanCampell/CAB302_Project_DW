82ae09625c13b57a8e8882c3e1a2f31051fd600b
Sean C <seancampbell_@outlook.com>
2024-04-14 17:12:20 +1000

    Added comments to App.java

--- src/main/java/com/example/apptrack/App.java
+++ src/main/java/com/example/apptrack/App.java
@@ -1,11 +1,13 @@
 package com.example.apptrack;
 
 public class App {
+    // Initial Variables
     private final String name;
     private final AppType type;
     private int timeUse;
     private int timeLimit;
 
+    // Constructor
     public App(String name, AppType type, int timeLimit) {
         this.name = name;
         this.type = type;
@@ -13,6 +15,7 @@ public class App {
         this.timeLimit = timeLimit;
     }
 
+    // Get and Set functions
     public String getName() {
         return name;
     }
@@ -37,10 +40,12 @@ public class App {
         this.timeLimit = timeLimit;
     }
 
+    // Add time that has been used on the application
     public void addTime(int time) {
         timeUse += time;
     }
 
+    // Check if time use has exceeded time limit
     public boolean isOverLimit() {
         if (timeLimit > 0) {
             return timeUse > timeLimit;
