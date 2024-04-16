9ebb530df53b20a21620adf1585019ee9f43af0f
Katharina Gremer <146542860+katharinalisa@users.noreply.github.com>
2024-04-17 02:32:17 +1000

    Update MainController.java

--- src/main/java/com/example/javafxreadingdemo/MainController.java
+++ src/main/java/com/example/javafxreadingdemo/MainController.java
@@ -49,14 +49,10 @@ public class MainController {
         timelimitButton.setToggleGroup(toggleGroup);
 
         homeButton.setSelected(true);
-
-        // Add a listener to the selectedToggleProperty of the ToggleGroup
         toggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue == null) {
-                // If no toggle is selected, clear the style
                 clearActiveButtonStyle();
             } else {
-                // If a toggle is selected, apply the active style to it
                 ((ToggleButton) newValue).getStyleClass().add("active-nav-button");
             }
         });
@@ -86,8 +82,6 @@ public class MainController {
     protected void onHomeButtonClick() throws IOException {
         clearActiveButtonStyle();
         homeButton.getStyleClass().add("active-nav-button");
-
-        // Show Home content
         homeContent.setVisible(true);
         insightsContent.setVisible(false);
         timeLimitsContent.setVisible(false);
@@ -97,8 +91,6 @@ public class MainController {
     protected void onInsightsButtonClick() throws IOException {
         clearActiveButtonStyle();
         insightButton.getStyleClass().add("active-nav-button");
-
-        // Show Insights content
         homeContent.setVisible(false);
         insightsContent.setVisible(true);
         timeLimitsContent.setVisible(false);
@@ -108,8 +100,6 @@ public class MainController {
     protected void onTimeLimitsButtonClick() throws IOException {
         clearActiveButtonStyle();
         timelimitButton.getStyleClass().add("active-nav-button");
-
-        // Show Time Limits content
         homeContent.setVisible(false);
         insightsContent.setVisible(false);
         timeLimitsContent.setVisible(true);
@@ -124,10 +114,7 @@ public class MainController {
 
     @FXML
     protected void onLogoutButtonClick() throws IOException {
-        // Clear the current user
         setCurrentUser(null);
-
-        // Redirect to the login view
         Stage stage = (Stage) logoutButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
